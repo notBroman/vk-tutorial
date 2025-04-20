53c3b66ebf80d63ff085932ff0d03c81946998f2
Roman Berger <romanberger001@gmail.com>
2025-04-20 22:55:39 +0100

    error with extensions

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ private:
   const uint32_t HEIGHT = 600;
   GLFWwindow *window;
 
+  VkInstance instance;
+
 public:
   void run() {
     initWindow();
@@ -29,7 +31,47 @@ private:
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
   }
 
-  void initVulkan() {}
+  void initVulkan() {
+    createInstance();
+  }
+  
+  void createInstance(){
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char*> requiredExtensions;
+
+    for(uint32_t i = 0; i < glfwExtensionCount; ++i){
+      requiredExtensions.emplace_back(glfwExtensions[i]);
+    }
+
+    requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATE_EXTENTION_NAME);
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    
+    createInfo.enabledExtensionCount = requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+    if (result != VK_SUCCESS){
+      throw std::runtime_error("failed to create VkInstance");
+    }
+
+  }
 
   void mainLoop() {
 
